54fa49f4e5a8ab275dadd652127032f8885e9a46
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2017-10-02 17:27:00 +0100

    a bit of classifying

---
+++ Classify.py
@@ -0,0 +1,97 @@
+#!/usr/bin/python
+
+# this starts to classify the output file we got back from
+# CensysIESMTP.py
+
+import sys
+import json
+
+def good(p25):
+    #print "good check: " + json.dumps(p25)
+    if 'smtp' not in p25:
+        return False
+    if 'starttls' not in p25['smtp']:
+        return False
+    if 'tls' not in p25['smtp']['starttls']:
+        return False
+    if 'validation' not in p25['smtp']['starttls']['tls']:
+        return False
+    if 'browser_trusted' not in p25['smtp']['starttls']['tls']['validation']:
+        return False
+    if p25['smtp']['starttls']['tls']['validation']['browser_trusted'] == True :
+        return True
+    else:
+        return False
+
+def medium(p25):
+    if 'smtp' not in p25:
+        return False
+    if 'starttls' not in p25['smtp']:
+        return False
+    if 'tls' not in p25['smtp']['starttls']:
+        return False
+    if 'signature' not in p25['smtp']['starttls']['tls']:
+        return False
+    if 'valid' not in p25['smtp']['starttls']['tls']['signature']:
+        return False
+    if p25['smtp']['starttls']['tls']['signature']['valid'] == True :
+        #print json.dumps(p25)
+        return True
+    else:
+        return False
+    
+def bad(p25):
+    if 'smtp' not in p25:
+        return True
+    if 'starttls' not in p25['smtp']:
+        return True
+    if 'tls' not in p25['smtp']['starttls']:
+        return True
+    else:
+        return False
+    
+
+with open(sys.argv[1],'r') as f:
+    f1=open('outs/good.json', 'w')
+    f2=open('outs/medium.json', 'w')
+    f3=open('outs/bad.json', 'w')
+    f4=open('outs/dunno.json', 'w')
+    f5=open('outs/exception.json', 'w')
+    overallcount=0
+    goodcount=0
+    mediumcount=0
+    badcount=0
+    dunnocount=0
+    exceptioncount=0
+    for line in f:
+        j_content = json.loads(line)
+        p25=j_content['p25']
+        try:
+            if good(p25):
+                f1.write(json.dumps(j_content) + '\n')
+                goodcount += 1
+            elif medium(p25):
+                f2.write(json.dumps(j_content) + '\n')
+                mediumcount += 1
+            elif bad(p25):
+                f3.write(json.dumps(j_content) + '\n')
+                badcount += 1
+            else:
+                f4.write(json.dumps(j_content) + '\n')
+                dunnocount += 1
+        except :
+            f5.write(json.dumps(j_content) + '\n')
+            exceptioncount += 1
+        overallcount += 1
+        if overallcount % 100 == 0:
+            # exit early for debug purposes
+            #break
+            print "Did : " + str(overallcount)
+        
+
+print "good: " + str(goodcount)
+print "medium: " + str(mediumcount)
+print "bad: " + str(badcount)
+print "dunno: " + str(dunnocount)
+print "exception: " + str(exceptioncount)
+print "overall: " + str(overallcount)
--- README.md
+++ README.md
@@ -3,3 +3,35 @@
 Code for various survey-related stuff
 
 Initial work is based on using the [censys.io](https://censys.io/) API
+
+## Cron job
+
+CensysSMTP-cron.py is set to pull a weekly view of the SMTP speakers,
+in my case from IE and EE (the latter for comparison).
+
+## Analysis
+
+Initially, I want to analyse the mail speakers into the following classes:
+
+- good-looking
+- medium-looking
+- bad-looking
+- dunno 
+- exceptions (in the try/catch sense)
+
+First cut will be to define these as: 
+
+- "good-looking" : Talks SMTPTLS with certs that chain to a browser-trusted root 
+- "medium-looking" : Talks STARTTLS ok but without a cert like the above
+- "bad-looking" : doesn't manage to talk STARTTLS for some reason
+- "dunno" : doesn't fit the above
+- "exceptions" : something else my code didn't catch, figure it out later
+
+Classify.py takes a filename ("f") as input and split that file into
+those classes i.e. good, medium, bad, dunno and exceptions, and will
+produce some basic counts. Those files are dropped into an "outs" 
+directory below the CWD for now.
+
+Later, we can try do some stats etc.
+
+
