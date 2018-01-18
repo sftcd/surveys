ec9aa24a6226089cfe02adaf0cbc910c67d92d14
Stephen Farrell <stephen@down.dsg.cs.tcd.ie>
2018-01-18 12:51:16 +0000

    gc

--- gc.sh
+++ gc.sh
@@ -31,24 +31,32 @@ then
 	exit -1
 fi
 
+# check if host is up at all first
+#ping -c 1 $HOST
+#if [ "$?" != "0" ]
+#then
+	#echo "$HOST not answering a ping"
+	#exit -1
+#fi
+
 OUTF=$HOST-$PORT.cert.txt
 
 echo "Getting cert for $HOST:$PORT"
 if [ "$PORT" == "443" ]
 then
-	echo | openssl s_client -connect $HOST:$PORT | openssl x509 -noout -text >$OUTF 2>/dev/null
+	echo | openssl s_client -connect $HOST:$PORT | openssl x509 -noout -text >$OUTF 
 fi
 if [ "$PORT" == "25" ]
 then
-	echo | openssl s_client -connect $HOST:$PORT -starttls smtp | openssl x509 -noout -text >$OUTF 2>/dev/null
+	echo | openssl s_client -connect $HOST:$PORT -starttls smtp | openssl x509 -noout -text >$OUTF 
 fi
 if [ "$PORT" == "143" ]
 then
-	echo | openssl s_client -connect $HOST:$PORT -starttls imap | openssl x509 -noout -text >$OUTF 2>/dev/null
+	echo | openssl s_client -connect $HOST:$PORT -starttls imap | openssl x509 -noout -text >$OUTF 
 fi
 if [ "$PORT" == "993" ]
 then
-	echo | openssl s_client -connect $HOST:$PORT | openssl x509 -noout -text >$OUTF 2>/dev/null
+	echo | openssl s_client -connect $HOST:$PORT | openssl x509 -noout -text >$OUTF 
 fi
 
 echo "Output is in $OUTF"
