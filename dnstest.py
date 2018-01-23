b4b819073491191d70f6fc59bb720c2ffdd3bddd
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2018-01-23 21:02:30 +0000

    tweaks

--- dnstest.py
+++ dnstest.py
@@ -14,9 +14,14 @@ rdns=''
 # caching means just do one at a time, not sure where the caches for these different
 # things are, but in real classifying uses, we'll almost all be cache misses
 
-test=sys.argv[2]
+sockettest=True
+try:
+    if sys.argv[2]=='dns':
+        sockettest=False
+except:
+    pass
 
-if test!='2':
+if sockettest:
 
     start=datetime.datetime.utcnow()
     try:
@@ -40,7 +45,7 @@ else:
     try:
         answers=dns.resolver.query(dnsname,"A")
         for rdata in answers:
-            print rdata
+            #print rdata
             ip=str(rdata)
     except Exception as e: 
         print >> sys.stderr, "dns exception " + str(e) + " for " + dnsname
@@ -51,11 +56,11 @@ else:
         req = '.'.join(reversed(ip.split("."))) + ".in-addr.arpa"
         answers = dns.resolver.query(req, "PTR")
         for rdata in answers:
-            print rdata
+            #print rdata
             rdns=str(rdata)
     except Exception as e: 
         print >> sys.stderr, "dns exception " + str(e) + " for " + dnsname
     end=datetime.datetime.utcnow()
     d2=end-start
-    print "socket calls: for " + dnsname + ": forward: " + str(d1) + " reverse: " + str(d2) + " [" + ip + "," + rdns + "]"
+    print "   dns calls: for " + dnsname + ": forward: " + str(d1) + " reverse: " + str(d2) + " [" + ip + "," + rdns + "]"
 
