df437a42396c67d1fa9ebd07fd248730402e95be
Stephen Farrell <stephen.farrell@cs.tcd.ie>
2018-02-23 22:19:53 +0000

    added manual fixer-upper script

---
+++ try-render-problematic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# GraphKeyReUse3.py produces a summary file at the end that
+# says which graphs couldn't be rendered for some reason
+# This script tries again for those
+
+# You should generally run this in the directory containing 
+# all the other graphs (outdir from GraphKeyReUse3) as that's
+# where the summary.txt file will be put
+
+list=`cat summary.txt | grep "not rendered" \
+		| sed -e 's/^.*\[//' \
+		| sed -e 's/\].*$//' \
+		| sed -e 's/, / /g'`
+
+for gr in $list
+do
+	echo "Trying $gr..."
+	# don't re-do already done stuff
+	target=graph$gr.dot.svg
+	if [ ! -f $target ]
+	then
+		sfdp -Tsvg graph$gr.dot >$target
+		if (( $? != 0 ))
+		then
+			mv $target failed-$ge.dot.svg
+		fi
+	fi
+done
+
