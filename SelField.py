1023abb3bf5018def119dfc81aebbcdeb9067ceb
Stephen Farrell <stephen@village.n4c.eu>
2018-03-04 11:55:12 +0000

    added own pretty print for censys.io formatted files

---
+++ SelField.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# for any file with one json-dict/line, read it line by line
+# decode and pretty-print it - thus allowing a less or grep
+# to be used to see what's what
+
+import sys
+import json
+import gc
+
+# install via  "$ sudo pip install -U jsonpickle"
+import jsonpickle
+
+jsonpickle.set_encoder_options('json', sort_keys=True, indent=2)
+with open(sys.argv[1],'r') as f:
+    goodcount=0
+    for line in f:
+        j_content = json.loads(line)
+        bstr=jsonpickle.encode(j_content,unpicklable=False)
+        print bstr
+
