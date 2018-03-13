cadfc3e3d95ee67d69911d1a641b8cab61ddbd0c
Stephen Farrell <stephen@hoba.ie>
2018-03-13 21:38:05 +0000

    added installer

--- README.md
+++ README.md
@@ -6,7 +6,8 @@ The current code collects and collates keys and sees which of those
 are the same. Needs proper documentation - that'll happen shortly
 is the plan:-) 
 
-- ```skey-all.sh``` is a script to orchestrate things.
+- ```install-deps.sh``` is a first cut at an installer for dependencies
+- ```skey-all.sh``` is a script to orchestrate things
 - ```Makefile``` can be used for bits'n'pieces in various ways
 - The ```misc``` directory has various bits and pieces knocked up along
 the way in case they prove handy later.
---
+++ install-deps.sh
@@ -0,0 +1,131 @@
+#!/bin/bash
+
+# Copyright (C) 2018 Stephen Farrell, stephen.farrell@cs.tcd.ie
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+
+#set -x
+
+startdir=`/bin/pwd`
+
+# this script will install the various bits'n'pieces needed to get this
+# survey stuff working
+# kinda loosely tested on a relatively clean ubuntu 16.04 - YMMV
+
+# This assumes you're a sudoer and likely works better if you don't have
+# to constantly enter the sudo password
+
+# This isn't efficiently idempotent - it'll redo the lot each time
+
+# start out in good state
+sudo apt-get update
+sudo apt-get -y upgrade
+
+# maybe we got this script some weird way...
+sudo apt-get -y install git
+if [ ! -d $HOME/code ]
+then
+	mkdir -p $HOME/code
+fi 
+if [ ! -d $HOME/code/surveys ]
+then
+	cd $HOME/code
+	git clone https://github.com/sftcd/surveys
+else
+	# may as well do an update
+	cd $HOME/code/surveys
+	git pull
+fi
+
+# make a place for stuff..
+for subdir in runs IE EE 
+do
+	if [ ! -d $HOME/data/smtp/$subdir ]
+	then
+		mkdir -p $HOME/data/smtp/$subdir
+	fi
+done
+
+# zmap is an install
+sudo apt-get -y install zmap
+
+# maxmind - enough to just use our update tool
+./mm_update.sh
+
+# install pip
+sudo apt-get -y install python-pip
+
+sudo -H pip install --upgrade pip
+ 
+# install python modules needed, these are currently imported modules
+# but seems like some are built-in or dragged-in via dependencies, we
+# leave the list as-in anyway as it does no harm other than some 
+# warnings
+sudo -H pip install  argparse
+sudo -H pip install  copy
+sudo -H pip install  datetime
+sudo -H pip install  dateutil 
+sudo -H pip install  gc
+sudo -H pip install  geoip2.database
+sudo -H pip install  graphviz 
+sudo -H pip install  import os
+sudo -H pip install  json
+sudo -H pip install  jsonpickle
+sudo -H pip install  pympler 
+sudo -H pip install  pytz 
+sudo -H pip install  socket
+sudo -H pip install  subprocess
+sudo -H pip install  sys
+sudo -H pip install  tempfile
+sudo -H pip install  time
+
+# golang, zmap and zgrab 
+# ubuntu's golang version isn't currently recent enough, need to go for
+# a stable version from upstream
+
+# better get wget
+sudo apt-get -y install wget
+
+mkdir -p $HOME/code/golang
+cd $HOME/code/golang
+GOTARBALL=go1.10.linux-amd64.tar.gz
+GOURL=https://dl.google.com/go/$GOTARBALL
+wget $GOURL
+tar xzvf $GOTARBALL
+sudo mv go /usr/lib/go-1.10
+sudo ln -sf /usr/lib/go-1.10 /usr/lib/go
+sudo ln -sf /usr/lib/go-1.10/bin/go /usr/bin/go
+
+# add GOPATH to .bashrc
+donealready=`grep GOPATH $HOME/.bashrc`
+if [[ "$donealready" == "" ]]
+then
+	echo "export GOPATH=$HOME/go" >>$HOME/.bashrc
+fi
+
+export GOPATH=$HOME/go
+
+# got get stuff
+go get github.com/zmap/zgrab
+cd $GOPATH/src/github.com/zmap/zgrab
+go build
+
+# clean up
+cd $starddir
+echo "Done! (I hope:-)"
