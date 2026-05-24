9ade88b187d920ae47d12e50bc89eeb2af04e69f
Teniola Malomo <malomot@tcd.ie>
2026-05-24 21:45:15 +0000

    swap zgrab for zgrab2 in FreshGrab

--- FreshGrab.py
+++ FreshGrab.py
@@ -63,19 +63,47 @@ country=def_country
 if args.country is not None:
     country=args.country
 
-# default timeout for zgrab, in seconds
-ztimeout=' -timeout 2'
+# default timeout for zgrab2, in seconds
+ztimeout=' -t 2s'
 
-# port parameters
-pparms={ 
-        '22': '-port 22 -xssh',
-        '25': '-port 25 -smtp -starttls -banners',
-        '110': '-port 110 -pop3 -starttls -banners',
-        '143': '-port 143 -imap -starttls -banners',
-        '443': '-port 443 -tls -http /',
-        '587': '-port 587 -smtp -starttls -banners',
-        '993': '-port 993 -imap -tls -banners',
+# port parameters (zgrab2 module syntax)
+pparms={
+        '22': 'ssh --port 22',
+        '25': 'smtp --port 25',
+        '110': 'pop3 --port 110 --starttls',
+        '143': 'imap --port 143 --starttls',
+        '443': 'http --port 443 --use-https',
+        '587': 'smtp --port 587',
+        '993': 'imap --port 993 --imaps',
         }
+
+def translate_v2(port, v2_result):
+    try:
+        data=v2_result.get('data',{})
+        module=next(iter(data))
+        result=data[module].get('result',{})
+    except Exception:
+        return v2_result
+    if 'tls' in result:
+        hl=result['tls'].get('handshake_log')
+        if hl is not None:
+            result['tls']=hl
+    v1={'ip': v2_result.get('ip','')}
+    if module == 'http':
+        try:
+            req=result['response']['request']
+            tl=req.get('tls_log',{}).get('handshake_log')
+            if tl is not None:
+                req['tls_handshake']=tl
+                del req['tls_log']
+        except (KeyError, TypeError):
+            pass
+        v1['data']={'http': result}
+    elif module == 'ssh':
+        v1['data']={'xssh': result}
+    else:
+        v1['data']=result
+    return v1
 
 def usage():
     print("usage: " + sys.argv[0] + " -i <infile> -o <putfile> [-p <portlist>] [-s <sleepsecs>]", file=sys.stderr)
@@ -165,14 +193,12 @@ with open(args.infile,'r') as f:
         jthing['writer']="FreshGrab.py"
         for port in ports:
             try:
-                cmd='zgrab '+  pparms[port] + ztimeout
+                cmd='zgrab2 '+  pparms[port] + ztimeout
                 proc=subprocess.Popen(cmd.split(),stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-                pc=proc.communicate(input=ip.encode())
-                lines=pc[0].decode("utf-8").split('\n')
-                jinfo=json.loads(lines[1])
-                jres=json.loads(lines[0])
-                #print jinfo
-                #print jres
+                pc=proc.communicate(input=(ip+',localhost').encode())
+                out=pc[0].decode("utf-8").strip()
+                jres=json.loads(out)
+                jres=translate_v2(port, jres)
                 jthing['p'+port]=jres
             except Exception as e:
                 # something goes wrong, we just record what
